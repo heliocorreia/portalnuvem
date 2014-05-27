905fd7ce825feba64867b7f134fc4a7d181187dd
Helio Correia <dev@heliocorreia.org>
2014-05-26 22:56:19 -0300

    single header extracted as content-hd

---
+++ portalnuvem/content-hd.php
@@ -0,0 +1,6 @@
+    <header class="content--hd">
+        <nav class="content--breadcrumb">
+            BREADCRUMB
+        </nav>
+        <h1 class="content--title"><?php the_title(); ?></h1>
+    </header>
--- portalnuvem/single.php
+++ portalnuvem/single.php
@@ -1,13 +1,7 @@
 <?php get_header(); the_post(); ?>
 
 <section class="single">
-    <header class="single--hd">
-        <nav class="single--breadcrumb">
-            BREADCRUMB
-        </nav>
-        <time class="single--datetime"><?php the_time('d M/Y'); ?></time>
-        <h1 class="single--title"><?php the_title(); ?></h1>
-    </header>
+    <?php get_template_part('content', 'hd'); ?>
 
     <div class="single--bd">
         <article class="single--content">
