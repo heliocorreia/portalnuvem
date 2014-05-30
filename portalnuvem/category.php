54d46b1561d53a536bbf7ea88ba40736bd4a73bf
Helio Correia <dev@heliocorreia.org>
2014-05-30 00:40:35 -0300

    category template

---
+++ portalnuvem/category.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<header class="content-hd">
+    <div class="content--container">
+        <?php get_template_part('partials/breadcrumb'); ?>
+        <h1 class="content-hd--title"><?php
+            if (in_category('eventos')):
+                print('Próximos Eventos');
+
+            elseif (in_category('noticias')):
+                print('Últimas');
+
+            elseif (in_category('colunas')):
+                print('Colunas');
+
+            endif;
+        ?></h1>
+    </div>
+</header>
+
+<div class="archive-bd">
+    <div class="archive-bd--container">
+        <article class="archive-bd--content">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()): the_post(); ?>
+                    <?php get_template_part('content', get_post_format()); ?>
+                <?php endwhile ?>
+            <?php endif ?>
+        </article>
+    </div>
+</div>
+
+<?php get_footer(); ?>
