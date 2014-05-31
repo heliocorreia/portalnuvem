0ac55ef2158533fb139572c9f71ddb7e911ade10
Helio Correia <dev@heliocorreia.org>
2014-05-31 17:14:31 -0300

    archive template for artist post type

---
+++ portalnuvem/archive-artist.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<header class="content-hd">
+    <div class="content--container">
+        <?php get_template_part('partials/breadcrumb'); ?>
+        <h1 class="content-hd--title">Artistas</h1>
+    </div>
+</header>
+
+<div class="archive-artist">
+    <div class="archive-artist--container">
+        <?php
+        query_posts(array(
+            'posts_per_page' => 100,
+            'post_type' => 'artist'
+        ));
+        if (have_posts()): ?>
+            <?php while (have_posts()): the_post(); ?>
+                <?php get_template_part('content', 'artist'); ?>
+            <?php endwhile ?>
+        <?php endif ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
