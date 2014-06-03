7e94f38add3f1f883c656c3aae83ad44bb65f7e4
Helio Correia <dev@heliocorreia.org>
2014-06-03 01:06:47 -0300

    article aside: author links

--- portalnuvem/single-article.php
+++ portalnuvem/single-article.php
@@ -15,6 +15,32 @@
             <?php the_content(); ?>
         </article>
         <aside class="single-article--aside">
+            <?php if ($photo284x284 = get_the_author_meta('photo284x284')): ?>
+            <figure class="single-article--aside-photo284x284">
+                <img src="<?php echo esc_attr($photo284x284); ?>" width="284" height="284" />
+            </figure>
+            <?php endif; ?>
+
+            <?php if ($description = get_the_author_meta('description')): ?>
+            <p class="single-article--aside-description"><?php echo $description; ?></p>
+            <?php endif; ?>
+
+            <?php
+            $user_url = get_the_author_meta('user_url');
+            $facebook = get_the_author_meta('facebook');
+            $twitter = get_the_author_meta('twitter');
+            $instagram = get_the_author_meta('instagram');
+
+            if ($user_url || $facebook || $twitter || $instagram):
+            ?>
+            <ul class="single-article--aside-links">
+                <?php if ($user_url): ?><li class="single-article--aside-site"><a href="#">Site</a></li><?php endif ?>
+                <?php if ($facebook): ?><li class="single-article--aside-facebook"><a href="#">Facebook</a></li><?php endif ?>
+                <?php if ($twitter): ?><li class="single-article--aside-twitter"><a href="#">Twitter</a></li><?php endif ?>
+                <?php if ($instagram): ?><li class="single-article--aside-instagram"><a href="#">Instagram</a></li><?php endif ?>
+            </ul>
+            <?php endif; ?>
+
             <h1>Textos Anteriores</h1>
             <ul>
                 <li><a href="#">Adasd asdas das ds</a></li>
