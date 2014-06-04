af9156a7ad3b933150a57f6604ddee85bf757686
Helio Correia <dev@heliocorreia.org>
2014-06-04 05:38:26 -0300

    partners links

--- portalnuvem/home.php
+++ portalnuvem/home.php
@@ -65,16 +65,16 @@
 <section class="home-partners">
     <h1 class="home-partners-title">Conheça os parceiros da nuvem</h1>
     <ul class="home-partners-items">
-        <li class="home-partners-item"><span class="uma-capsula">Uma Cápsula</span></li>
-        <li class="home-partners-item"><span class="casa-da-luz">Casa da Luz</span></li>
-        <li class="home-partners-item"><span class="doutro-jeito">D'Outro Jeito</span></li>
-        <li class="home-partners-item"><span class="maquina">Maquina</span></li>
-        <li class="home-partners-item"><span class="layer-28">?????</span></li>
-        <li class="home-partners-item"><span class="contra">Contra</span></li>
-        <li class="home-partners-item"><span class="acidum">Acidum</span></li>
-        <li class="home-partners-item"><span class="imaginaria">Imaginária</span></li>
-        <li class="home-partners-item"><span class="layer-32">?????</span></li>
-        <li class="home-partners-item"><span class="schizzi-books">Schizzi Books</span></li>
+        <li class="home-partners-item"><a href="http://umacapsula.com/" class="uma-capsula">Uma Cápsula</a></li>
+        <li class="home-partners-item"><a href="http://www.casadaluz.net" class="casa-da-luz">Casa da Luz</a></li>
+        <li class="home-partners-item"><a href="http://www.facebook.com/doutrojeito" class="doutro-jeito">D'Outro Jeito</a></li>
+        <li class="home-partners-item"><a href="http://www.maquina.art.br/" class="maquina">Maquina</a></li>
+        <li class="home-partners-item"><a href="http://mionline.com.br/" class="mi">Mi</a></li>
+        <li class="home-partners-item"><a href="http://contra.cc/" class="contra">Contra</a></li>
+        <li class="home-partners-item"><a href="http://www.grupoacidum.art.br" class="acidum">Acidum</a></li>
+        <li class="home-partners-item"><a href="http://www.imaginaria.cc" class="imaginaria">Imaginária</a></li>
+        <li class="home-partners-item"><a href="http://cargocollective.com/raulluna" class="cargo-collective">Cargo Collective</a></li>
+        <li class="home-partners-item"><a href="http://www.zupi.com.br" class="schizzi-books">Schizzi Books</a></li>
     </ul>
 </section>
 
