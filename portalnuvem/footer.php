850ef70c92a718a5fb1d62cc691806f295ec4f90
Helio Correia <dev@heliocorreia.org>
2014-06-04 22:59:37 -0300

    extra main footer

--- portalnuvem/footer.php
+++ portalnuvem/footer.php
@@ -21,6 +21,18 @@
             <p id="ft--copy">Nuvem Produções <?php echo date('Y')?> - Todos os direitos reservados</p>
             <p id="ft--made-in">Feito em Recife - Pernambuco</p>
         </div>
+        <div id="ft--xtra">
+            <div class="container">
+                <dl class="sponsorship">
+                    <dt class="incentive">Incentivo:</dt>
+                    <dd class="entity funcultura"><a class="entity-entry" href="http://www.pe.gov.br/programas/funcultura/">Funcultura</a></dd>
+                    <dd class="entity fundarpe"><a class="entity-entry" href="http://www.pe.gov.br/orgaos/fundarpe-fundacao-do-patrimonio-historico-e-artistico-de-pernambuco/">Fundarpe</a></dd>
+                    <dd class="entity secretaria-pe"><a class="entity-entry" href="http://www.pe.gov.br/">Secretaria de Cultura</a></dd>
+                    <dt class="realization">Realização:</dt>
+                    <dd class="entity nuvem-gm"><span class="entity-entry" >Nuvem <span>Guga Marques</span></span></dd>
+                </dl>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
