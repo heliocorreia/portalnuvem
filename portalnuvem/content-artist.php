214bb34894915b576071c836cc13807213de1530
Helio Correia <dev@heliocorreia.org>
2014-06-11 04:43:01 -0300

    content artist hover

--- portalnuvem/content-artist.php
+++ portalnuvem/content-artist.php
@@ -1,6 +1,6 @@
 <section class="artist--entry">
     <figure class="artist--entry-figure">
-        <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('348x213') ?></a>
+        <a class="artist--entry-figure-link" href="<?php the_permalink() ?>"><?php the_post_thumbnail('348x213') ?></a>
     </figure>
     <h1 class="artist--entry-title"><a class="artist--entry-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
 </section>
