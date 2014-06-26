b91e673343a0c2c8b52d99e0fb646c5a07636df2
Helio Correia <dev@heliocorreia.org>
2014-06-26 00:03:12 -0300

    artist--entry-title suffix: meta value _artist_state

--- portalnuvem/content-artist.php
+++ portalnuvem/content-artist.php
@@ -2,5 +2,10 @@
     <figure class="artist--entry-figure">
         <a class="artist--entry-figure-link" href="<?php the_permalink() ?>"><?php the_post_thumbnail('348x213') ?></a>
     </figure>
-    <h1 class="artist--entry-title"><a class="artist--entry-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+    <h1 class="artist--entry-title"><a class="artist--entry-link" href="<?php the_permalink() ?>"><?php
+        the_title();
+        if ($state = get_post_meta($post->ID, '_artist_state', true)) {
+            echo " ($state)";
+        }
+    ?></a></h1>
 </section>
